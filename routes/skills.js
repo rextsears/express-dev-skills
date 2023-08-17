5a6be5fa3a898fe3b642f713fe544f1e0cec8c3e
rextsears <srtm88@gmail.com>
2023-08-17 01:55:44 -0400

    Lab Part 1 - update

--- routes/skills.js
+++ routes/skills.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-// Require the controller that exports To-Do CRUD functions
 var skillsCtrl = require('../controllers/skills');
 
-// All actual paths start with "/todos"
-
-// GET /todos
 router.get('/', skillsCtrl.index);
 
 module.exports = router;
